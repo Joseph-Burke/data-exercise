9dffcea726e45ec09f757647e44987b4b6087d5c
EstebanP <epinczinger@gmail.com>
2020-11-26 11:41:51 -0300

    process form

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-let game = { "name": "Jueguito" };
+let game = { name: "Jueguito" };
 
-const gameId = "UcKKRKvukMYoZMUP4puG"; 
+const gameId = "UcKKRKvukMYoZMUP4puG";
 
 const fetchScores = () => {
   return fetch(
     `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`
-  )
-    .then((response) => response.json())
-}
+  ).then((response) => response.json());
+};
 
-const postScore = score => {
+const postScore = (score) => {
   fetch(
     `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`,
     {
@@ -19,34 +18,49 @@ const postScore = score => {
       },
       body: JSON.stringify(score),
     }
-  )
+  );
 };
 
 async function displayScores() {
-  let scores = await fetchScores()
+  let scores = await fetchScores();
   let scoresArray = scores.result.sort((a, b) => {
-    return b.score - a.score
+    return b.score - a.score;
   });
 
-  let list = document.getElementById('scores-list');
+  let list = document.getElementById("scores-list");
 
-  scoresArray.forEach( score => {
-    let listItem = document.createElement('li');
-    listItem.textContent = `${score.user}: ${score.score}`
+  scoresArray.forEach((score) => {
+    let listItem = document.createElement("li");
+    listItem.textContent = `${score.user}: ${score.score}`;
     list.appendChild(listItem);
   });
-};
+}
 
 function refreshScores() {
-  let list = document.getElementById('scores-list');
-  list.innerHTML = '';
+  let list = document.getElementById("scores-list");
+  list.innerHTML = "";
   displayScores();
 }
 
-const refreshButton = document.getElementById('refresh-scores-button');
+const refreshButton = document.getElementById("refresh-scores-button");
 
-refreshButton.addEventListener('click', function() {
-  refreshScores()
+refreshButton.addEventListener("click", function () {
+  refreshScores();
 });
 
 displayScores();
+
+async function processForm () {
+  let user = document.getElementById("user-input");
+  let score = document.getElementById("score-input");
+
+  let newInput = { user: user.value, score: score.value };
+  
+  postScore(newInput);
+  refreshScores();
+};
+
+let button = document.getElementById("submit-btn");
+button.addEventListener("click", () => {
+  processForm();
+});
