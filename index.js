51861d55b2c91d210e6dc5b367032b6aaf6d4a6c
EstebanP <epinczinger@gmail.com>
2020-11-26 10:14:19 -0300

    add get results method

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-let game = { "name": "testgame" };
+let game = { "name": "Jueguito" };
 
-fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games", {
-  method: "POST",
-  headers: {
-    "content-type": "application/json"
-  },
-  body: JSON.stringify(game)
-})
-.then(response => response.json())
-.then(data => console.log(data))
+const gameId = "UcKKRKvukMYoZMUP4puG"; 
+
+// fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games", {
+//   method: "POST",
+//   headers: {
+//     "content-type": "application/json"
+//   },
+//   body: JSON.stringify(game)
+// })
+// .then(response => response.json())
+// .then(data => console.log(data))
+
+const gameResults = () => {
+  return fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`
+  )
+    .then((response) => response.json())
+}
+
+gameResults().then(data => console.log(data));
